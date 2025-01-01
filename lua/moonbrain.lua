af29fe6bcf00a6ee8266bc2e7cfc4052cd4745f8
Kishan S Patel <bishan.batel@protonmail.com>
2025-01-01 00:44:17 -0800

    Lsp Integration

--- lua/moonbrain.lua
+++ lua/moonbrain.lua
@@ -25,6 +25,40 @@ M.setup = function()
 	})
 
 	require("nvim-web-devicons").set_icon_by_filetype({ moonbrain = "moonbrain" })
+	local client = nil
+
+	-- @return integer?
+	local function start_client()
+		return vim.lsp.start_client({
+			name = "gooslsp",
+			cmd = { "/home/bishan_/code/moonbrain/target/debug/meteor-lsp" },
+		})
+	end
+
+	local augroup = vim.api.nvim_create_augroup("MoonBrain", {})
+
+	vim.api.nvim_create_autocmd({ "FileType" }, {
+		pattern = "*.moonbrain",
+		command = [[set ft=moonbrain]],
+		group = augroup,
+	})
+
+	vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+		pattern = "*.moonbrain",
+		callback = function()
+			if not client then
+				client = start_client()
+			end
+
+			if not client then
+				vim.notify("client be ded lol")
+			else
+				vim.notify("Client")
+				vim.lsp.buf_attach_client(0, client)
+			end
+		end,
+		group = augroup,
+	})
 end
 
 return M
--- plugin/moonbrain.lua
+++ plugin/moonbrain.lua
@@ -8,14 +8,6 @@ require("nvim-treesitter").setup()
 local api = vim.api
 
 -- define autocommands
-local augroup = api.nvim_create_augroup("MoonBrain", {})
-
 vim.cmd([[ autocmd BufNewFile,BufRead *.moonbrain set filetype=moonbrain ]])
 
--- vim.api.nvim_create_autocmd({ "BufNewFile, BufRead" }, {
--- 	pattern = "*.moonbrain",
--- 	command = [[set ft=moonbrain]],
--- 	group = augroup,
--- })
---
 require("moonbrain").setup()
